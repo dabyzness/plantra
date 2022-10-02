516531f78887f5d28fb6b9094c82d564ba3c8050
Damian <bzdyrad@gmail.com>
2022-10-02 19:01:22 -0400

    create noteSchema. add noteSchema array as property of personalPlantSchema

--- models/profile.js
+++ models/profile.js
@@ -3,26 +3,45 @@ import { Plant } from "./plant.js";
 
 const Schema = mongoose.Schema;
 
-const personalPlantSchema = new Schema({
-  plant: { type: Schema.Types.ObjectId, ref: "Plant" },
-  nickname: { type: String, trim: true, maxLength: 25 },
-  wateringSchedule: {
-    type: Number,
-    min: 0,
-    max: 28,
-    default: 7,
-  },
-  isWatered: {
-    type: Boolean,
-    default: false,
-  },
-  nextWater: {
-    type: Date,
-    default: function () {
-      return new Date().setDate(new Date().getDate() + this.wateringSchedule);
+const noteSchema = new Schema(
+  {
+    action: {
+      type: String,
+      default: "Watered",
+      required: true,
+    },
+    info: {
+      type: String,
+      default: "",
     },
   },
-});
+  { timestamps: true }
+);
+
+const personalPlantSchema = new Schema(
+  {
+    plant: { type: Schema.Types.ObjectId, ref: "Plant" },
+    nickname: { type: String, trim: true, maxLength: 25 },
+    wateringSchedule: {
+      type: Number,
+      min: 0,
+      max: 28,
+      default: 7,
+    },
+    isWatered: {
+      type: Boolean,
+      default: false,
+    },
+    nextWater: {
+      type: Date,
+      default: function () {
+        return new Date().setDate(new Date().getDate() + this.wateringSchedule);
+      },
+    },
+    notes: [noteSchema],
+  },
+  { timestamps: true }
+);
 
 const profileSchema = new Schema(
   {
