f6b4c4eeba7fa826a09d0d765ee84fd68934870d
Damian <bzdyrad@gmail.com>
2022-10-01 20:57:16 -0400

    refactor personalPlantSchema to include a watering Schedule

--- models/profile.js
+++ models/profile.js
@@ -6,6 +6,22 @@ const Schema = mongoose.Schema;
 const personalPlantSchema = new Schema({
   plant: { type: Schema.Types.ObjectId, ref: "Plant" },
   nickname: { type: String, trim: true, maxLength: 25 },
+  wateringSchedule: {
+    type: Number,
+    min: 0,
+    max: 30,
+    default: 7,
+  },
+  isWatered: {
+    type: Boolean,
+    default: false,
+  },
+  nextWater: {
+    type: Date,
+    default: function () {
+      return new Date().setDate(new Date().getDate() + this.wateringSchedule);
+    },
+  },
 });
 
 const profileSchema = new Schema(
