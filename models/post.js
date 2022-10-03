7ca4189b4ea6c2b81ccd9df7ffffb3d7328cf76e
Damian <bzdyrad@gmail.com>
2022-10-03 13:56:24 -0400

    create Posts model

---
+++ models/post.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { Profile } from "./profile.js";
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    owner: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
+    img: {
+      type: String,
+      default: "/assets/images/test.jpg",
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export { Post };
